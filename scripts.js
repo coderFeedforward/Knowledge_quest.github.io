77b83eb5e64e6fe7158127bf67e4b47f0ea71156
coderFeedforward <68401751+coderFeedforward@users.noreply.github.com>
2020-10-17 17:34:04 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-var x =3;
+
+function myFunction() {
+  var x = document.getElementById("myNumber").value;
+  var y = document.getElementById("myOtherNumber").value;
+  var z = (x * 1) + (y * 1 ); 
+
+  document.getElementById("demo").innerHTML = z;
+}
+
+
