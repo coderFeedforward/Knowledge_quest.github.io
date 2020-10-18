be4f5f92c7d1604ccc3b8b362a85b9da9318bece
coderFeedforward <68401751+coderFeedforward@users.noreply.github.com>
2020-10-18 14:22:22 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
 
 
-var myList = [null, null];
+//var myList = [null, null];
 function myFunction() {
   var x = document.getElementById("myNumber").value;
   var y = document.getElementById("myOtherNumber").value;
   var z = (x * 1) + (y * 1 ); 
-  myList[0] = z;
+  //myList[0] = z;
   
   document.getElementById("demo").innerHTML = z;
 }
 
-
+/*
 function Function() {
   var x = document.getElementById("myText").value;
   myList[1] = x;
   document.getElementById("demooo").innerHTML = x ;
 }
+*/
